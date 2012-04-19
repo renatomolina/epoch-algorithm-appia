340bc48e84bc5fc667a2e748403d3055bab18d1b
Renato Molina Toth <renatomolinat@gmail.com>
2012-04-19 14:50:42 -0300

    epoch

---
+++ src/layer/ApplicationLayer.java
@@ -0,0 +1,30 @@
+package layer;
+
+import net.sf.appia.core.Layer;
+import net.sf.appia.core.Session;
+import net.sf.appia.core.events.channel.ChannelInit;
+import session.ApplicationSession;
+import event.TrustEvent;
+
+public class ApplicationLayer extends Layer {
+
+  public ApplicationLayer() {
+    /* events that the protocol will create */
+    evProvide = new Class[0];
+    
+    /*
+     * events that the protocol requires to work This is a subset of the
+     * accepted events.
+     */
+    evRequire = new Class[0];
+
+    /* events that the protocol will accept */
+    evAccept = new Class[2];
+    evAccept[0] = TrustEvent.class;
+    evAccept[1] = ChannelInit.class;
+  }
+
+  public Session createSession() {
+    return new ApplicationSession(this);
+  }
+}
